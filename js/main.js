c9f80163b9ff1e963778424f262872dfc8702825
Rus-ka <rrkaziev@gmail.com>
2022-09-02 00:01:59 +0300

    Added start button
    Added start button and prompt to choose quantity of cards. Looks ugly but it works :)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 import Card from './card.js'
-console.log(Card);
+
+
+// const selectors = {
+//     boardContainer: document.querySelector('.board-container'),
+//     board: document.querySelector('.board'),
+//     moves: document.querySelector('.moves'),
+//     timer: document.querySelector('.timer'),
+//     start: document.querySelector('button'),
+//     win: document.querySelector('.win')
+// }
+
+// const state = {
+//     gameStarted: false,
+//     flippedCards: 0,
+//     totalFlips: 0,
+//     totalTime: 0,
+//     loop: null
+// }
+
+const startButton = document.getElementById('start_button');
+
+startButton.addEventListener('click', () => {
+    let doc = prompt("Please enter number of pairs");
+      
+    if (((doc % 2) == 0) || (doc.value >= 0 )|| (doc.value < 10)) {
+     newGame(document.getElementById('game'), `${doc}`);
+        } 
+        else {
+            alert("значение для поля установлено по умолчанию (4)");
+            newGame(document.getElementById('game'), 4);
+            }  
+});
 
 
 function newGame(container, cardsCount){
-   //Сrated field
+   //Сreated field
     let cardsNumberArray  = [],
     cardsArray =[],
     firstCard = null,
@@ -53,8 +84,9 @@ function newGame(container, cardsCount){
         }
         //reset
         if(document.querySelectorAll('.card.success').length == cardsNumberArray.length){
-            alert('pobeda');
-            container.innerHTML = '';
+           // alert('pobeda');
+            container.innerHTML = `<span class="win-text">
+            You won!<br /></span>`,
             cardsNumberArray  = [],
             cardsArray =[],
             firstCard = null,
@@ -67,4 +99,6 @@ function newGame(container, cardsCount){
 
  }
 // newGame(cardsCount);
- newGame(document.getElementById('game'), 6);
+// newGame(document.getElementById('game'), 6);]
+
+
